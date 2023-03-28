e3784a79863ed5650f8404160fb1bda76b5877e2
apple <apple@apples-MacBook-Pro.local>
2023-03-28 21:52:54 +0500

    feat(): working on food module

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -3,66 +3,73 @@ import { CreateVendorInput } from "../dtos";
 import { Vendor } from "../models";
 import { GeneratePassword, GenerateSalt } from "../utility";
 
-// refactor code 
+// refactor code
 // email is an optional
-export const FindVendor = async (id: string | undefined, email?: string )  => {
-  if(email) {
-    return await Vendor.findOne({email: email})
+export const FindVendor = async (id: string | undefined, email?: string) => {
+  if (email) {
+    return await Vendor.findOne({ email: email });
   } else {
-    return await Vendor.findById(id)
+    return await Vendor.findById(id);
   }
-}
+};
 
 export const createVendor = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const {
-    name,
-    ownerName,
-    foodType,
-    address,
-    pincode,
-    password,
-    phone,
-    email,
-  } = <CreateVendorInput>req.body;
+  try {
+    const {
+      name,
+      ownerName,
+      foodType,
+      address,
+      pincode,
+      password,
+      phone,
+      email,
+    } = <CreateVendorInput>req.body;
 
-  // check already exist vendor
-  const alreadyExist = await FindVendor("", email)
+    // check already exist vendor
+    const alreadyExist = await FindVendor("", email);
 
-  // if it exist thro the response
-  if (alreadyExist) {
-    return res
-      .status(409)
-      .json({ success: false, message: "vendor already exist" });
+    // if it exist thro the response
+    if (alreadyExist) {
+      return res
+        .status(409)
+        .json({ success: false, message: "vendor already exist" });
+    }
+
+    // generate salt
+    const salt = await GenerateSalt();
+
+    // encrypt the password
+    const hashPassword = await GeneratePassword(password, salt);
+
+    // create new vendor
+    const vendor = await Vendor.create({
+      name,
+      ownerName,
+      foodType,
+      address,
+      pincode,
+      password: hashPassword,
+      phone,
+      email,
+      salt: salt,
+      rating: 0,
+      serviceAvailabilty: false,
+      coverImage: [],
+    });
+
+    // send a response in json format
+    res.status(201).json({ success: true, data: vendor });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
   }
-
-  // generate salt
-  const salt = await GenerateSalt();
-
-  // encrypt the password
-  const hashPassword = await GeneratePassword(password, salt);
-
-  // create new vendor
-  const vendor = await Vendor.create({
-    name,
-    ownerName,
-    foodType,
-    address,
-    pincode,
-    password: hashPassword,
-    phone,
-    email,
-    salt: salt,
-    rating: 0,
-    serviceAvailabilty: false,
-    coverImage: [],
-  });
-
-  // send a response in json format
-  res.status(201).json({ success: true, data: vendor });
 };
 
 export const getVendors = async (
@@ -70,16 +77,25 @@ export const getVendors = async (
   res: Response,
   next: NextFunction
 ) => {
-  // find all vendors
-  const vendors = await Vendor.find({}).lean();
+  try {
+    // find all vendors
+    const vendors = await Vendor.find({}).lean();
 
-  // if no vendor exists
-  if (!vendors.length) {
-    return res.status(404).json({ success: false, message: "no vendor exist" });
+    // if no vendor exists
+    if (!vendors.length) {
+      return res
+        .status(404)
+        .json({ success: false, message: "no vendor exist" });
+    }
+
+    // send a response in json format
+    res.status(200).json({ success: true, data: vendors });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
   }
-
-  // send a response in json format
-  res.status(200).json({ success: true, data: vendors });
 };
 
 export const getVendorById = async (
@@ -87,21 +103,28 @@ export const getVendorById = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  if (!id) {
-    return res
-      .status(400)
-      .json({ success: false, message: "id is missing in params" });
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "id is missing in params" });
+    }
+
+    const vendor = await FindVendor(id);
+
+    if (!vendor) {
+      return res
+        .status(400)
+        .json({ success: false, message: "no vendor found" });
+    }
+    // send a response in json format
+    res.status(200).json({ success: true, data: vendor });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
   }
-
-  const vendor = await FindVendor(id);
-
-  if (!vendor) {
-    return res
-      .status(400)
-      .json({ success: false, message: "no vendor found" });
-  }
-  // send a response in json format
-  res.status(200).json({ success: true, data: vendor });
 };
--- src/controllers/vendor.controller.ts
+++ src/controllers/vendor.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { VendorLoginInput } from "../dtos";
-import { Vendor } from "../models";
+import { EditVendorInput, VendorLoginInput } from "../dtos";
+import { CreateFoodInput } from "../dtos/food.dto";
+import { Food } from "../models";
 import { GenerateSignature, ValidatePassword } from "../utility";
 import { FindVendor } from "./admin.controller";
 
@@ -9,43 +10,50 @@ export const vendorLogin = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { email, password } = <VendorLoginInput>req.body;
+  try {
+    const { email, password } = <VendorLoginInput>req.body;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ success: false, message: "missing values in payload" });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "missing values in payload" });
+    }
+
+    const alreadyExist = await FindVendor("", email);
+
+    if (!alreadyExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "invaild credentails" });
+    }
+
+    const validate = await ValidatePassword(
+      password,
+      alreadyExist.password,
+      alreadyExist.salt
+    );
+
+    if (!validate) {
+      return res
+        .status(400)
+        .json({ success: false, message: "invaild credentails" });
+    }
+
+    // generate signature
+    const signature = GenerateSignature({
+      _id: alreadyExist._id,
+      name: alreadyExist.name,
+      email: alreadyExist.email,
+      foodType: alreadyExist.foodType,
+    });
+
+    res.status(200).json({ success: true, token: signature });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
   }
-
-  const alreadyExist = await FindVendor("", email);
-
-  if (!alreadyExist) {
-    return res
-      .status(400)
-      .json({ success: false, message: "invaild credentails" });
-  }
-
-  const validate = await ValidatePassword(
-    password,
-    alreadyExist.password,
-    alreadyExist.salt
-  );
-
-  if (!validate) {
-    return res
-      .status(400)
-      .json({ success: false, message: "invaild credentails" });
-  }
-
-  // generate signature
-  const signature = GenerateSignature({
-    _id: alreadyExist._id,
-    name: alreadyExist.name,
-    email: alreadyExist.email,
-    foodType: alreadyExist.foodType,
-  });
-
-  res.status(200).json({ success: true, token: signature });
 };
 
 export const getVendorProfile = async (
@@ -53,26 +61,143 @@ export const getVendorProfile = async (
   res: Response,
   next: NextFunction
 ) => {
-  const user = req.user;
+  try {
+    const user = req.user;
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "vendor Information Not Found" });
+    }
 
-  if (!user) {
-    return res
-      .status(400)
-      .json({ success: false, message: "vendor Information Not Found" });
+    const existingVendor = await FindVendor(user._id);
+    res.status(200).json({ success: true, data: existingVendor });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
   }
-
-  const existingVendor = await FindVendor(user._id);
-  res.status(200).json({ success: true, data: existingVendor });
 };
 
 export const updateVendorProfile = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    const user = req.user;
+    const { foodType, name, address, phone } = <EditVendorInput>req.body;
+
+    if (!user) {
+      return res.status(400).json({
+        sucess: false,
+        message: "Unable to Update vendor profile",
+      });
+    }
+
+    const existingVendor = await FindVendor(user._id);
+
+    if (existingVendor !== null) {
+      existingVendor.name = name || existingVendor.name;
+      existingVendor.address = address || existingVendor.address;
+      existingVendor.phone = phone || existingVendor.phone;
+      existingVendor.foodType = foodType || existingVendor.foodType;
+      const saveResult = await existingVendor.save();
+
+      res.status(200).json({
+        sucess: true,
+        data: saveResult,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
+  }
+};
 
 export const updateVendorService = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    const user = req.user;
+
+    if (!user) {
+      return res.status(400).json({
+        sucess: false,
+        message: "Unable to Update vendor profile",
+      });
+    }
+
+    const existingVendor = await FindVendor(user._id);
+
+    if (existingVendor !== null) {
+      existingVendor.serviceAvailabilty = !existingVendor.serviceAvailabilty;
+
+      const saveResult = await existingVendor.save();
+
+      res.status(200).json({
+        sucess: true,
+        data: saveResult,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
+  }
+};
+
+export const AddFood = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = req.user;
+    const { name, description, category, foodType, readyTime, price } = <
+      CreateFoodInput
+    >req.body;
+
+    if (!user) {
+      return res.status(400).json({
+        sucess: false,
+        message: "Unable to Update vendor profile",
+      });
+    }
+
+    const vendor = await FindVendor(user._id);
+
+    if (vendor !== null) {
+      const food = await Food.create({
+        vendorId: vendor._id,
+        name: name,
+        description: description,
+        category: category,
+        price: price,
+        rating: 0,
+        readyTime: readyTime,
+        foodType: foodType,
+        images: ["image.jpg"],
+      });
+
+      vendor.foods.push(food);
+      const result = await vendor.save();
+      return res.json(result);
+    } else {
+      return res.status(404).json({
+        sucess: false,
+        message: "vendor not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message ? error.message : "Internal server error",
+    });
+  }
+};
